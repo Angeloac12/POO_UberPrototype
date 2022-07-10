48bc245cd700defc28dafd6424d10e8dfe721918
AngeloCaro <90576621+Angeloac12@users.noreply.github.com>
2022-07-10 13:52:49 -0500

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-//var car = new Car("AWR346", new Account("Andres Herrera", "123"));
-//car.passenger = 4;
-//car.printDataCar() ;
-var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
-car.passenger = 4;
-car.printDataCar();
